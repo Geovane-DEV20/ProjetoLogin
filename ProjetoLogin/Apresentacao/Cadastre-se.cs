60fd39a977678289569611f5ca5a51d3e211c2f7
Geovane-DEV20 <geovaneDEV1998@gmail.comm>
2021-09-03 23:39:12 -0300

    Criação da parte visual do sistema de Login

--- ProjetoLogin/Apresentacao/Cadastre-se.cs
+++ ProjetoLogin/Apresentacao/Cadastre-se.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ProjetoLogin
+namespace ProjetoLogin.Apresentacao
 {
-    public partial class Form1 : Form
+    public partial class Cadastre_se : Form
     {
-        public Form1()
+        public Cadastre_se()
         {
             InitializeComponent();
         }
@@ -22,12 +22,12 @@ namespace ProjetoLogin
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void label3_Click(object sender, EventArgs e)
         {
 
         }
--- ProjetoLogin/Apresentacao/Form1.Designer.cs
+++ ProjetoLogin/Apresentacao/Form1.Designer.cs
@@ -29,13 +29,130 @@ namespace ProjetoLogin
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.textbLogin = new System.Windows.Forms.TextBox();
+            this.textbSenha = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnCadastre = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Location = new System.Drawing.Point(237, 137);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(75, 23);
+            this.btnEntrar.TabIndex = 1;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseMnemonic = false;
+            this.btnEntrar.UseVisualStyleBackColor = false;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(325, 137);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.Location = new System.Drawing.Point(173, 56);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(57, 23);
+            this.lblLogin.TabIndex = 3;
+            this.lblLogin.Text = "Login:";
+            this.lblLogin.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(168, 94);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(63, 23);
+            this.lblSenha.TabIndex = 4;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // textbLogin
+            // 
+            this.textbLogin.Location = new System.Drawing.Point(237, 59);
+            this.textbLogin.Name = "textbLogin";
+            this.textbLogin.Size = new System.Drawing.Size(164, 20);
+            this.textbLogin.TabIndex = 5;
+            // 
+            // textbSenha
+            // 
+            this.textbSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textbSenha.Location = new System.Drawing.Point(237, 97);
+            this.textbSenha.Name = "textbSenha";
+            this.textbSenha.PasswordChar = '*';
+            this.textbSenha.Size = new System.Drawing.Size(163, 22);
+            this.textbSenha.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(33, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnCadastre
+            // 
+            this.btnCadastre.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastre.Location = new System.Drawing.Point(237, 178);
+            this.btnCadastre.Name = "btnCadastre";
+            this.btnCadastre.Size = new System.Drawing.Size(163, 32);
+            this.btnCadastre.TabIndex = 8;
+            this.btnCadastre.Text = "CADASTRE-SE";
+            this.btnCadastre.UseVisualStyleBackColor = true;
+            this.btnCadastre.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(476, 261);
+            this.Controls.Add(this.btnCadastre);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textbSenha);
+            this.Controls.Add(this.textbLogin);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnEntrar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.DimGray;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox textbLogin;
+        private System.Windows.Forms.TextBox textbSenha;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnCadastre;
     }
 }
 
